f83211fbf1e0e1633552faf651c3032b14271c25
Ganesh Bhat <ganeshsurfs@gmail.com>
2020-11-11 20:21:16 +0530

    Adding minor changes to data and logs folder and adding config for electron. Change as per use

--- index.js
+++ index.js
@@ -46,6 +46,28 @@ async function createWindow() {
     })
 }
 
+app.setPath('temp', './tmp')
+app.setPath('cache', './cache')
+app.setPath('downloads', './downloads')
+app.setPath('userData', './data/user')
+app.setPath('logs', './data/logs')
+app.setPath('crashDump', './data/crash')
+app.setPath('recent', './data/recent')
+
+// Specify common AppData
+app.setPath('appData', './data/app')
+
+// Alternatively, use following AppData path based on OS
+// aix, darwin, freebsd, linux, openbsd, sunos, win32
+
+// if (process.platform === 'win32') {
+//     app.setPath('appData', '%APPDATA%')
+// } else if (process.platform === 'darwin') {
+//     app.setPath('appData', '$XDG_CONFIG_HOME')
+// } else if (process.platform === 'linux') {
+//     app.setPath('appData', '~/Library/Application Support')
+// }
+
 app.on('ready', createWindow.bind(proxyapp, sampleapp));
 app.on('window-all-closed', function () {
     if (process.platform !== 'darwin') {
