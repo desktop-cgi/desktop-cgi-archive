9133b75acf636a7335ca8b7cf99ad878591eac5c
Ganesh K Bhat <ganeshsurfs@gmail.com>
2022-09-19 21:24:49 -0700

    [COMMIT] Adding mocha.test.config.js file and test script for mocha

---
+++ test/src/test.template.js
@@ -0,0 +1,83 @@
+
+var expect = require('chai').expect;
+
+describe('test.template.js::desktop-cgi: Test Suite for - ', function() {
+
+    describe ('test.template.js::desktop-cgi: [Test A] Test Suite for ', function() {
+        // it('[Test A] package.json Present', function(done){
+        //     expect(200).to.equal(200);
+        //     done();
+        // });
+
+        it('[Test A] ', function(done) {
+            expect(100).to.equal(100);
+            done();
+        });
+
+    });
+
+    describe ('test.template.js::desktop-cgi: [Test B] Test Suite for ', function() {
+        // it('[Test B] status', function(done){
+        //     expect(200).to.equal(200);
+        //     done();
+        // });
+
+        it('[Test B] ', function(done) {
+            expect(100).to.equal(100);
+            done();
+        });
+
+    });
+
+    describe ('test.template.js::desktop-cgi: [Test C] Test Suite for ', function() {
+        // it('[Test C] status', function(done){
+        //     expect(200).to.equal(200);
+        //     done();
+        // });
+
+        it('[Test C] ', function(done) {
+            expect(100).to.equal(100);
+            done();
+        });
+
+    });
+
+    describe ('test.template.js::desktop-cgi: [Test D] Test Suite for ', function() {
+        // it('[Test D] status', function(done){
+        //     expect(200).to.equal(200);
+        //     done();
+        // });
+
+        it('[Test D] ', function(done) {
+            expect(100).to.equal(100);
+            done();
+        });
+
+    });
+
+    describe ('test.template.js::desktop-cgi: [Test E] Test Suite Negatives for ', function() {
+        // it('[Test E] status', function(done){
+        //     expect(200).to.equal(200);
+        //     done();
+        // });
+
+        it('[Test E] ', function(done) {
+            expect(100).to.equal(100);
+            done();
+        });
+
+    });
+
+    describe ('test.template.js::desktop-cgi: [Test F] Test Suite Negatives for ', function() {
+        // it('[Test F] status', function(done){
+        //     expect(200).to.equal(200);
+        //     done();
+        // });
+
+        it('[Test F] ', function(done) {
+            expect(100).to.equal(100);
+            done();
+        });
+
+    });
+});
